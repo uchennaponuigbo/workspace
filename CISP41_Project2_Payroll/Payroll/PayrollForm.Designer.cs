880360bc99b6e32748f97485b7f934c1ddd8b0ff
bonuigbo <onuigma@gmail.com>
2016-10-06 15:50:25 -0700

    Added initial payroll project

---
+++ CISP41_Project2_Payroll/Payroll/PayrollForm.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Payroll
+{
+    partial class PayrollForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.employeeNameTextBox = new System.Windows.Forms.TextBox();
+            this.hoursWorkedTextBox = new System.Windows.Forms.TextBox();
+            this.hourlyRateTextBox = new System.Windows.Forms.TextBox();
+            this.deductionCodeTextBox = new System.Windows.Forms.TextBox();
+            this.grossPayTextBox = new System.Windows.Forms.TextBox();
+            this.taxesTextBox = new System.Windows.Forms.TextBox();
+            this.deductionTextBox = new System.Windows.Forms.TextBox();
+            this.netPayTextBox = new System.Windows.Forms.TextBox();
+            this.employeeNameLabel = new System.Windows.Forms.Label();
+            this.hoursWorkedLabel = new System.Windows.Forms.Label();
+            this.hourlyRateLabel = new System.Windows.Forms.Label();
+            this.deductionCodeLabel = new System.Windows.Forms.Label();
+            this.grossPayLabel = new System.Windows.Forms.Label();
+            this.taxesLabel = new System.Windows.Forms.Label();
+            this.deductionLabel = new System.Windows.Forms.Label();
+            this.netPayLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.summaryButton = new System.Windows.Forms.Button();
+            this.employeeNameValidLabel = new System.Windows.Forms.Label();
+            this.hoursWorkedValidLabel = new System.Windows.Forms.Label();
+            this.hourlyRateValidLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // employeeNameTextBox
+            // 
+            this.employeeNameTextBox.Location = new System.Drawing.Point(46, 111);
+            this.employeeNameTextBox.Name = "employeeNameTextBox";
+            this.employeeNameTextBox.Size = new System.Drawing.Size(186, 22);
+            this.employeeNameTextBox.TabIndex = 2;
+            // 
+            // hoursWorkedTextBox
+            // 
+            this.hoursWorkedTextBox.Location = new System.Drawing.Point(46, 202);
+            this.hoursWorkedTextBox.MaximumSize = new System.Drawing.Size(200, 40);
+            this.hoursWorkedTextBox.Name = "hoursWorkedTextBox";
+            this.hoursWorkedTextBox.Size = new System.Drawing.Size(186, 22);
+            this.hoursWorkedTextBox.TabIndex = 3;
+            // 
+            // hourlyRateTextBox
+            // 
+            this.hourlyRateTextBox.Location = new System.Drawing.Point(46, 303);
+            this.hourlyRateTextBox.Name = "hourlyRateTextBox";
+            this.hourlyRateTextBox.Size = new System.Drawing.Size(186, 22);
+            this.hourlyRateTextBox.TabIndex = 4;
+            // 
+            // deductionCodeTextBox
+            // 
+            this.deductionCodeTextBox.Location = new System.Drawing.Point(46, 403);
+            this.deductionCodeTextBox.Name = "deductionCodeTextBox";
+            this.deductionCodeTextBox.Size = new System.Drawing.Size(186, 22);
+            this.deductionCodeTextBox.TabIndex = 5;
+            // 
+            // grossPayTextBox
+            // 
+            this.grossPayTextBox.Location = new System.Drawing.Point(399, 111);
+            this.grossPayTextBox.Name = "grossPayTextBox";
+            this.grossPayTextBox.Size = new System.Drawing.Size(186, 22);
+            this.grossPayTextBox.TabIndex = 6;
+            // 
+            // taxesTextBox
+            // 
+            this.taxesTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.taxesTextBox.Location = new System.Drawing.Point(399, 202);
+            this.taxesTextBox.Name = "taxesTextBox";
+            this.taxesTextBox.Size = new System.Drawing.Size(186, 22);
+            this.taxesTextBox.TabIndex = 7;
+            // 
+            // deductionTextBox
+            // 
+            this.deductionTextBox.Location = new System.Drawing.Point(399, 303);
+            this.deductionTextBox.Name = "deductionTextBox";
+            this.deductionTextBox.Size = new System.Drawing.Size(186, 22);
+            this.deductionTextBox.TabIndex = 8;
+            // 
+            // netPayTextBox
+            // 
+            this.netPayTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.netPayTextBox.ForeColor = System.Drawing.Color.Green;
+            this.netPayTextBox.Location = new System.Drawing.Point(399, 403);
+            this.netPayTextBox.Name = "netPayTextBox";
+            this.netPayTextBox.ReadOnly = true;
+            this.netPayTextBox.Size = new System.Drawing.Size(186, 22);
+            this.netPayTextBox.TabIndex = 9;
+            // 
+            // employeeNameLabel
+            // 
+            this.employeeNameLabel.AutoSize = true;
+            this.employeeNameLabel.Location = new System.Drawing.Point(43, 90);
+            this.employeeNameLabel.Name = "employeeNameLabel";
+            this.employeeNameLabel.Size = new System.Drawing.Size(111, 17);
+            this.employeeNameLabel.TabIndex = 10;
+            this.employeeNameLabel.Text = "Employee Name";
+            // 
+            // hoursWorkedLabel
+            // 
+            this.hoursWorkedLabel.AutoSize = true;
+            this.hoursWorkedLabel.Location = new System.Drawing.Point(43, 182);
+            this.hoursWorkedLabel.Name = "hoursWorkedLabel";
+            this.hoursWorkedLabel.Size = new System.Drawing.Size(99, 17);
+            this.hoursWorkedLabel.TabIndex = 12;
+            this.hoursWorkedLabel.Text = "Hours Worked";
+            // 
+            // hourlyRateLabel
+            // 
+            this.hourlyRateLabel.AutoSize = true;
+            this.hourlyRateLabel.Location = new System.Drawing.Point(43, 283);
+            this.hourlyRateLabel.Name = "hourlyRateLabel";
+            this.hourlyRateLabel.Size = new System.Drawing.Size(83, 17);
+            this.hourlyRateLabel.TabIndex = 14;
+            this.hourlyRateLabel.Text = "Hourly Rate";
+            // 
+            // deductionCodeLabel
+            // 
+            this.deductionCodeLabel.AutoSize = true;
+            this.deductionCodeLabel.Location = new System.Drawing.Point(45, 383);
+            this.deductionCodeLabel.Name = "deductionCodeLabel";
+            this.deductionCodeLabel.Size = new System.Drawing.Size(109, 17);
+            this.deductionCodeLabel.TabIndex = 16;
+            this.deductionCodeLabel.Text = "Deduction Code";
+            // 
+            // grossPayLabel
+            // 
+            this.grossPayLabel.AutoSize = true;
+            this.grossPayLabel.Location = new System.Drawing.Point(396, 90);
+            this.grossPayLabel.Name = "grossPayLabel";
+            this.grossPayLabel.Size = new System.Drawing.Size(74, 17);
+            this.grossPayLabel.TabIndex = 18;
+            this.grossPayLabel.Text = "Gross Pay";
+            // 
+            // taxesLabel
+            // 
+            this.taxesLabel.AutoSize = true;
+            this.taxesLabel.Location = new System.Drawing.Point(396, 182);
+            this.taxesLabel.Name = "taxesLabel";
+            this.taxesLabel.Size = new System.Drawing.Size(46, 17);
+            this.taxesLabel.TabIndex = 20;
+            this.taxesLabel.Text = "Taxes";
+            // 
+            // deductionLabel
+            // 
+            this.deductionLabel.AutoSize = true;
+            this.deductionLabel.Location = new System.Drawing.Point(396, 283);
+            this.deductionLabel.Name = "deductionLabel";
+            this.deductionLabel.Size = new System.Drawing.Size(72, 17);
+            this.deductionLabel.TabIndex = 22;
+            this.deductionLabel.Text = "Deduction";
+            // 
+            // netPayLabel
+            // 
+            this.netPayLabel.AutoSize = true;
+            this.netPayLabel.Location = new System.Drawing.Point(396, 383);
+            this.netPayLabel.Name = "netPayLabel";
+            this.netPayLabel.Size = new System.Drawing.Size(58, 17);
+            this.netPayLabel.TabIndex = 24;
+            this.netPayLabel.Text = "Net Pay";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(46, 466);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(124, 28);
+            this.calculateButton.TabIndex = 25;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(188, 466);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(124, 28);
+            this.clearButton.TabIndex = 26;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(522, 466);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(124, 28);
+            this.exitButton.TabIndex = 27;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // summaryButton
+            // 
+            this.summaryButton.Location = new System.Drawing.Point(379, 466);
+            this.summaryButton.Name = "summaryButton";
+            this.summaryButton.Size = new System.Drawing.Size(124, 28);
+            this.summaryButton.TabIndex = 28;
+            this.summaryButton.Text = "Summary";
+            this.summaryButton.UseVisualStyleBackColor = true;
+            this.summaryButton.Click += new System.EventHandler(this.summaryButton_Click);
+            // 
+            // employeeNameValidLabel
+            // 
+            this.employeeNameValidLabel.AutoSize = true;
+            this.employeeNameValidLabel.ForeColor = System.Drawing.Color.Red;
+            this.employeeNameValidLabel.Location = new System.Drawing.Point(43, 136);
+            this.employeeNameValidLabel.Name = "employeeNameValidLabel";
+            this.employeeNameValidLabel.Size = new System.Drawing.Size(0, 17);
+            this.employeeNameValidLabel.TabIndex = 29;
+            // 
+            // hoursWorkedValidLabel
+            // 
+            this.hoursWorkedValidLabel.AutoSize = true;
+            this.hoursWorkedValidLabel.ForeColor = System.Drawing.Color.Red;
+            this.hoursWorkedValidLabel.Location = new System.Drawing.Point(45, 227);
+            this.hoursWorkedValidLabel.MaximumSize = new System.Drawing.Size(250, 44);
+            this.hoursWorkedValidLabel.Name = "hoursWorkedValidLabel";
+            this.hoursWorkedValidLabel.Size = new System.Drawing.Size(0, 17);
+            this.hoursWorkedValidLabel.TabIndex = 30;
+            // 
+            // hourlyRateValidLabel
+            // 
+            this.hourlyRateValidLabel.AutoSize = true;
+            this.hourlyRateValidLabel.ForeColor = System.Drawing.Color.Red;
+            this.hourlyRateValidLabel.Location = new System.Drawing.Point(45, 328);
+            this.hourlyRateValidLabel.Name = "hourlyRateValidLabel";
+            this.hourlyRateValidLabel.Size = new System.Drawing.Size(0, 17);
+            this.hourlyRateValidLabel.TabIndex = 31;
+            // 
+            // PayrollForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 527);
+            this.Controls.Add(this.hourlyRateValidLabel);
+            this.Controls.Add(this.hoursWorkedValidLabel);
+            this.Controls.Add(this.employeeNameValidLabel);
+            this.Controls.Add(this.summaryButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.netPayLabel);
+            this.Controls.Add(this.deductionLabel);
+            this.Controls.Add(this.taxesLabel);
+            this.Controls.Add(this.grossPayLabel);
+            this.Controls.Add(this.deductionCodeLabel);
+            this.Controls.Add(this.hourlyRateLabel);
+            this.Controls.Add(this.hoursWorkedLabel);
+            this.Controls.Add(this.employeeNameLabel);
+            this.Controls.Add(this.netPayTextBox);
+            this.Controls.Add(this.deductionTextBox);
+            this.Controls.Add(this.taxesTextBox);
+            this.Controls.Add(this.grossPayTextBox);
+            this.Controls.Add(this.deductionCodeTextBox);
+            this.Controls.Add(this.hourlyRateTextBox);
+            this.Controls.Add(this.hoursWorkedTextBox);
+            this.Controls.Add(this.employeeNameTextBox);
+            this.Name = "PayrollForm";
+            this.Text = "Payroll";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox employeeNameTextBox;
+        private System.Windows.Forms.TextBox hoursWorkedTextBox;
+        private System.Windows.Forms.TextBox hourlyRateTextBox;
+        private System.Windows.Forms.TextBox deductionCodeTextBox;
+        private System.Windows.Forms.TextBox grossPayTextBox;
+        private System.Windows.Forms.TextBox taxesTextBox;
+        private System.Windows.Forms.TextBox deductionTextBox;
+        private System.Windows.Forms.TextBox netPayTextBox;
+        private System.Windows.Forms.Label employeeNameLabel;
+        private System.Windows.Forms.Label hoursWorkedLabel;
+        private System.Windows.Forms.Label hourlyRateLabel;
+        private System.Windows.Forms.Label deductionCodeLabel;
+        private System.Windows.Forms.Label grossPayLabel;
+        private System.Windows.Forms.Label taxesLabel;
+        private System.Windows.Forms.Label deductionLabel;
+        private System.Windows.Forms.Label netPayLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button summaryButton;
+        private System.Windows.Forms.Label employeeNameValidLabel;
+        private System.Windows.Forms.Label hoursWorkedValidLabel;
+        private System.Windows.Forms.Label hourlyRateValidLabel;
+    }
+}
+
---
+++ CISP41_Project2_Payroll/Payroll/PayrollForm.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Payroll
+{
+    public partial class PayrollForm : Form
+    {
+        private int _totalEmployeesProcessed;
+        private decimal _totalGrossPay;
+        private decimal _totalDeduction;
+        private decimal _totalTaxes;
+        private decimal _totalNetPay;
+
+        private const decimal OvertimeRate = 1.5m;
+        private const decimal DoubleOvertimeRate = 2.0m;
+        private const decimal TaxRate = 0.25m;
+        private const decimal MinimunWage = 9.25m;
+
+        private List<string> _employeeSummaryInfo;
+
+        public PayrollForm()
+        {
+            _employeeSummaryInfo = new List<string>();
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            // Clear out validation messages
+            employeeNameValidLabel.Text = "";
+            hourlyRateValidLabel.Text = "";
+            hoursWorkedValidLabel.Text = "";
+
+            bool formIsValid = true;
+
+            employeeNameValidLabel.Text = "";
+            string employeeName = employeeNameTextBox.Text;
+
+            // Validate that the employee is filled in properly
+            if (employeeNameTextBox.Text == "")
+            {
+                employeeNameValidLabel.Text = "Please enter an employee name";
+                formIsValid = false;
+            }
+
+            // Validate that the hours are proper
+            int hoursWorked;
+            bool hoursWorkedIsValid = Int32.TryParse(hoursWorkedTextBox.Text, out hoursWorked);
+
+            if (hoursWorkedTextBox.Text == "")
+            {
+                hoursWorkedValidLabel.Text = "Please enter the number of hours worked";
+                formIsValid = false;
+            }
+            else if (!hoursWorkedIsValid)
+            {
+                hoursWorkedValidLabel.Text = "Please enter a valid number for number of hours worked.";
+                formIsValid = false;
+            }
+            else if (hoursWorked > 70 || hoursWorked < 5)
+            {
+                hoursWorkedValidLabel.Text = "The hours must be between 5 and 70.";
+                formIsValid = false;
+            }
+
+            // Validate that the hourly rate is proper
+            decimal hourlyRate;
+            bool hourlyRateIsValid = Decimal.TryParse(hourlyRateTextBox.Text, out hourlyRate);
+            if (hourlyRateTextBox.Text == "")
+            {
+                hourlyRateValidLabel.Text = "Please enter the rate.";
+                formIsValid = false;
+            }
+            else if (!hourlyRateIsValid)
+            {
+                hourlyRateValidLabel.Text = "Please enter a valid number for the rate.";
+                formIsValid = false;
+            }
+            else if (hourlyRate < MinimunWage)
+            {
+                hourlyRateValidLabel.Text = "The rate must be greater than 9.25.";
+                formIsValid = false;
+            }
+
+            // Do not process anything if the fields above are invalid
+            if (!formIsValid)
+            {
+                return;
+            }
+
+            decimal deduction = calculateDeduction(deductionCodeTextBox.Text);
+            if(deduction == 0m)
+            {
+                // If the user entered something invalid or blank, set the text box to show the correct value
+                deductionCodeTextBox.Text = "D0";
+            }
+
+            decimal grossPay = calculateGrossPay(hoursWorked, hourlyRate);
+            decimal taxes = (grossPay - deduction) * TaxRate;
+            decimal netPay = grossPay - deduction - taxes;
+
+            // Output all the calculated variables to the Form
+            grossPayTextBox.Text = grossPay.ToString("N2");
+            deductionTextBox.Text = deduction.ToString("N2");
+            taxesTextBox.Text = taxes.ToString("N2");
+            netPayTextBox.Text = netPay.ToString("N2");
+
+            // Finally, update all the total values
+            _totalEmployeesProcessed++;
+            _totalGrossPay += grossPay;
+            _totalDeduction += deduction;
+            _totalTaxes += taxes;
+            _totalNetPay += netPay;
+
+            // Optional - update employee summary info
+            _employeeSummaryInfo.Add(employeeName + ": ".PadRight(30 - employeeName.Length, ' ') + netPay.ToString("N2"));
+        }
+
+        private decimal calculateDeduction(string deductionCode)
+        {
+            // This is the default value, if the user enters anything not valid, or D0
+            decimal deduction = 0m;
+            if (deductionCodeTextBox.Text == "D1")
+            {
+                deduction = 10m;
+            }
+            else if (deductionCodeTextBox.Text == "D2")
+            {
+                deduction = 30m;
+            }
+            else if (deductionCodeTextBox.Text == "D3")
+            {
+                deduction = 60m;
+            }
+            else
+            {
+                // Any invalid input will default to 0, and we will display the proper deduction code
+                deduction = 0m;
+                deductionCodeTextBox.Text = "D0";
+            }
+            return deduction;
+
+        }
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            employeeNameTextBox.Text = "";
+            hoursWorkedTextBox.Text = "";
+            hourlyRateTextBox.Text = "";
+            deductionCodeTextBox.Text = "";
+
+            employeeNameValidLabel.Text = "";
+            hourlyRateValidLabel.Text = "";
+            hoursWorkedValidLabel.Text = "";
+
+            grossPayTextBox.Text = "";
+            deductionTextBox.Text = "";
+            taxesTextBox.Text = "";
+            netPayTextBox.Text = "";
+
+            _totalEmployeesProcessed = 0;
+            _totalGrossPay = 0m;
+            _totalDeduction = 0m;
+            _totalTaxes = 0m;
+            _totalNetPay = 0m;
+
+            _employeeSummaryInfo.Clear();
+
+        }
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void summaryButton_Click(object sender, EventArgs e)
+        {
+            var messageString = "";
+            messageString += "# of Processed Employees: " + _totalEmployeesProcessed.ToString();
+            messageString += "\nTotal Gross Pay: " + _totalGrossPay.ToString("N2");
+            messageString += "\nTotal Deduction: " + _totalDeduction.ToString("N2");
+            messageString += "\nTotal Taxes: " + _totalTaxes.ToString("N2");
+            messageString += "\nTotal Net Pay: " + _totalNetPay.ToString("N2");
+            messageString += "\n\n";
+
+            for(int i = 0; i < _employeeSummaryInfo.Count; i++)
+            {
+                messageString += "\n" + _employeeSummaryInfo[i];
+            }
+
+            MessageBox.Show(messageString, "Summary", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private decimal calculateGrossPay(int hoursWorked, decimal hourlyRate)
+        {
+            int overTimeHours = hoursWorked - 40;
+            if (overTimeHours < 0)
+            {
+                overTimeHours = 0;
+            }
+
+            int doubleOverTimeHours = hoursWorked - 60;
+            if (doubleOverTimeHours < 0)
+            {
+                doubleOverTimeHours = 0;
+            }
+
+            // Make sure to only calculate a maximum of 40 hours worked            
+            if(hoursWorked > 40)
+            {
+                hoursWorked = 40;
+            }
+
+            decimal regularHoursPay = hoursWorked * hourlyRate;
+            decimal overTimePay = overTimeHours * hourlyRate * OvertimeRate;
+            decimal doubleOverTimePay = doubleOverTimeHours * hourlyRate * DoubleOvertimeRate;
+
+            decimal grossPay = regularHoursPay + overTimePay + doubleOverTimePay;
+
+            return grossPay;
+        }
+    }
+}
